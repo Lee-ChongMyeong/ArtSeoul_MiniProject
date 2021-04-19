9d9e8379a66c867e8e9ec987eda9fb7fc673bb87
cleverlee <dlchdaud1327@gmail.com>
2021-04-19 15:26:36 +0900

    테스트중

--- routers/commentRouter.js
+++ routers/commentRouter.js
@@ -10,27 +10,25 @@ commentRouter.get('/:boardId', async (req, res, next) => {
 	const boardId = req.params.boardId;
 	let result = { status: 'success', comments: [] };
 	try {
-		let comments = await CommentBoard.find({ boardId: boardId }).sort({ date: -1 });
-
-		for (comment of comments) {
-			console.log(comment)
-			let profileData = await User.findOne({ id :comment["userId"] })
-			console.log(profileData)
-			let temp = {
-				commentId: comment.commentId,
-				commentContents: comment.commentContents,
-				nickname: comment.nickname,
-                boardId : boardId,
-				userId: comment.userId,
-				profile : profileData["profile"]
-
-			};
-			result['comments'].push(temp);
-		}
-	} catch (err) {
-		result['status'] = 'fail';
-	}
-	res.json(result);
+	   let comments = await CommentBoard.find({ boardId: boardId }).sort({ date: -1 });
+ 
+	   for (comment of comments) {
+		// a = comment["userId"]
+		const b = await User.findOne({id:comment["userId"]},{profile:1});
+		let temp = {
+		   commentId: comment.commentId,
+		   commentContents: comment.commentContents,
+		   nickname: comment.nickname,
+			boardId : boardId,
+		   userId: comment.userId,
+		   profile:b
+		};
+		result['comments'].push(temp);
+	 }
+  } catch (err) {
+	 result['status'] = 'fail';
+  }
+  res.json(result);
 });
 
 
